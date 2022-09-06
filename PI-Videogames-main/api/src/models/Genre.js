adb5b61b5fc6c62fe4208a36483a5d4312eb5007
apuggioni <agustinpuggioni98@gmail.com>
2022-09-06 01:53:02 -0300

    routes working

---
+++ PI-Videogames-main/api/src/models/Genre.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Genre",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -1,12 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      released: {
+        type: DataTypes.DATEONLY,
+        defaultValue: DataTypes.NOW,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+      },
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
